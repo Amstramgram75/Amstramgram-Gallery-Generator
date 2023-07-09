99fcc0b596b4c29e7fec36748c13049a06b40b7b
Amstramgram75 <laurentneumann@free.fr>
2023-07-09 17:43:27 +0200

    Initial

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -55,114 +55,113 @@ const
     ]
   }, babelModule)
 
-if (process.env.BUILD === 'package') {
-  //Create dist if it does not exist, 
+if (process.env.BUILD === 'production') {
   fsExtra.ensureDirSync(dist)
   fsExtra.emptyDirSync(dist)
-} else {
-  if (process.env.BUILD === 'production') {
-    //Create prod/assets if it does not exist, 
-    fsExtra.ensureDirSync(`${prod}assets/`)
-    const
-      { res, err } = await reflect({
-        src: `${dev}assets/`,
-        dest: `${prod}assets/`,
-      }),
-      msg = err ? `\x1b[31m${err}` : `\x1b[33m${res}`//Directory "dev/assets" reflected to "prod/assets"
-    console.log(msg)
-    //Emptying all prod files and folders except assets directory
-    fsExtra.readdirSync(prod).filter(file => file != 'assets').forEach(file => fsExtra.removeSync(path.resolve(prod + file)))
-  }
-
-  //FIRST ROLLUP TASK :
-  //- bundle index.js in a module
-  //- compile css (with minification if in production)
-  //- compile html (with minification if in production)
-  //- minify js if in production
-  theExports.push(
-    {
-      input: `${src}js/index.js`,
-      output:
-      {
-        file: `${dest}js/index.js`,
-        format: 'esm',
-        sourcemap: process.env.BUILD === 'development',
-      },
-      plugins: [
-        noderesolve(),
-        commonjs(),
-        babel(babelModule),
-        //main.css
-        css({
-          jobs: { from: `${src}css`, to: `${dest}css` },
-          sourcemap: process.env.BUILD === 'development',
-          plugins: [
-            postcssImport(),
-            postcssPresetEnv({ stage: 1 }),
-            ...(process.env.BUILD === 'production' ? [cssnano()] : [])
-          ]
-        }),
-        //index.html
-        html({
-          jobs: { from: `${src}index.html/`, to: dest },
-          watch: true,
-          plugins: [
-            htmlinclude({
-              root: `${src}html/`
-            }),
-            ...(process.env.BUILD === 'production' ? [htmlnano()] : [])
-          ]
-        }),
-        copy({
-          targets: [
-            //minify polyfills/polyfills.js
-            {
-              src: `${src}js/polyfills/polyfills.js`,
-              dest: `${dest}js/polyfills/`,
-              transform: content => minifyJs(content),
-              rename: (name, extension) => `${name}.min.${extension}`
-            }
-          ]
-        }),
-        ...(process.env.BUILD === 'development' ?
-          //DEVELOPMENT
-          [
-          ]
-          :
-          //PRODUCTION : minify js
-          [
-            terser()
-          ]
-        )
-      ],
-      //Comment/Uncomment if you need
-      watch: {
-        clearScreen: process.env.BUILD === 'production',
-      },
-    }
-  )
-
-  //SECOND ROLLUP TASK : bundle index.js in IIFE format
-  //for usage in index.html
-  theExports.push(
-    {
-      input: `${src}js/index.js`,
-      output: {
-        file: `${dest}js/noModule/index.js`,
-        format: 'iife'
-      },
-      watch: {
-        clearScreen: process.env.BUILD === 'production'
-      },
-      plugins: [
-        noderesolve(),
-        commonjs(),
-        babel(babelNoModule),
-        ...(process.env.BUILD === 'production' ? [terser()] : []),
-      ]
-    }
-  )
+  fsExtra.copySync(`${app}amstramgramGalleryGenerator.mjs`, `${dist}amstramgramGalleryGenerator.mjs`)
+  fsExtra.copySync(`${app}package.json`, `${dist}package.json`)
+  fsExtra.copySync(`readme.md`, `${dist}readme.md`)
+  //Create prod/assets if it does not exist, 
+  fsExtra.ensureDirSync(`${prod}assets/`)
+  const
+    { res, err } = await reflect({
+      src: `${dev}assets/`,
+      dest: `${prod}assets/`,
+    }),
+    msg = err ? `\x1b[31m${err}` : `\x1b[33m${res}`//Directory "dev/assets" reflected to "prod/assets"
+  console.log(msg)
+  //Emptying all prod files and folders except assets directory
+  fsExtra.readdirSync(prod).filter(file => file != 'assets').forEach(file => fsExtra.removeSync(path.resolve(prod + file)))
 }
+
+//FIRST ROLLUP TASK :
+//- bundle index.js in a module
+//- compile css (with minification if in production)
+//- compile html (with minification if in production)
+//- minify js if in production
+theExports.push(
+  {
+    input: `${src}js/index.js`,
+    output:
+    {
+      file: `${dest}js/index.js`,
+      format: 'esm',
+      sourcemap: process.env.BUILD === 'development',
+    },
+    plugins: [
+      noderesolve(),
+      commonjs(),
+      babel(babelModule),
+      //main.css
+      css({
+        jobs: { from: `${src}css`, to: `${dest}css` },
+        sourcemap: process.env.BUILD === 'development',
+        plugins: [
+          postcssImport(),
+          postcssPresetEnv({ stage: 1 }),
+          ...(process.env.BUILD === 'production' ? [cssnano()] : [])
+        ]
+      }),
+      //index.html
+      html({
+        jobs: { from: `${src}index.html/`, to: dest },
+        watch: true,
+        plugins: [
+          htmlinclude({
+            root: `${src}html/`
+          }),
+          ...(process.env.BUILD === 'production' ? [htmlnano()] : [])
+        ]
+      }),
+      copy({
+        targets: [
+          //minify polyfills/polyfills.js
+          {
+            src: `${src}js/polyfills/polyfills.js`,
+            dest: `${dest}js/polyfills/`,
+            transform: content => minifyJs(content),
+            rename: (name, extension) => `${name}.min.${extension}`
+          }
+        ]
+      }),
+      ...(process.env.BUILD === 'development' ?
+        //DEVELOPMENT
+        [
+        ]
+        :
+        //PRODUCTION : minify js
+        [
+          terser()
+        ]
+      )
+    ],
+    //Comment/Uncomment if you need
+    watch: {
+      clearScreen: process.env.BUILD === 'production',
+    },
+  }
+)
+
+//SECOND ROLLUP TASK : bundle index.js in IIFE format
+//for usage in index.html
+theExports.push(
+  {
+    input: `${src}js/index.js`,
+    output: {
+      file: `${dest}js/noModule/index.js`,
+      format: 'iife'
+    },
+    watch: {
+      clearScreen: process.env.BUILD === 'production'
+    },
+    plugins: [
+      noderesolve(),
+      commonjs(),
+      babel(babelNoModule),
+      ...(process.env.BUILD === 'production' ? [terser()] : []),
+    ]
+  }
+)
 
 //Export rollup tasks
 export default theExports
